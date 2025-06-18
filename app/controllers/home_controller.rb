0ec8255b7210823bed2466e512aa391f462889f2
Daniele Costantini <d.costantini@donboscosandona.it>
2025-06-18 09:59:04 +0200

    footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,47 +8,82 @@ class HomeController < ApplicationController
   {
     name: 'Voli',
     href: '',
-    submenu: [
-      { name: 'Brasile', href: '/prodotto/antiacidi' },
-      { name: 'Argentina', href: '/prodotto/digestivi' },
-      { name: 'Perù', href: '/prodotto/antiacidi' },
-      { name: 'Colombia', href: '/prodotto/digestivi' },
-      { name: 'Cile', href: '/prodotto/altro' }
-    ]
+
   },
   {
     name: 'Hotel',
     href: '',
-    submenu: [
-      { name: 'Brasile', href: '/prodotto/antiacidi' },
-      { name: 'Argentina', href: '/prodotto/digestivi' },
-      { name: 'Perù', href: '/prodotto/antiacidi' },
-      { name: 'Colombia', href: '/prodotto/digestivi' },
-      { name: 'Cile', href: '/prodotto/altro' }
-    ]
+
   },
   {
-    name: 'Attrazioni',
+    name: 'Prenota biglietti',
     href: '',
-        submenu: [
-      { name: 'Brasile', href: '/prodotto/antiacidi' },
-      { name: 'Argentina', href: '/prodotto/digestivi' },
-      { name: 'Perù', href: '/prodotto/antiacidi' },
-      { name: 'Colombia', href: '/prodotto/digestivi' },
-      { name: 'Cile', href: '/prodotto/altro' }
-    ]
+
   },
   {
     name: 'Noleggia un veicolo',
     href: '',
-        submenu: [
-      { name: 'Brasile', href: '/prodotto/antiacidi' },
-      { name: 'Argentina', href: '/prodotto/digestivi' },
-      { name: 'Perù', href: '/prodotto/antiacidi' },
-      { name: 'Colombia', href: '/prodotto/digestivi' },
-      { name: 'Cile', href: '/prodotto/altro' }
-    ]
+
   }
+]
+
+        @footer = [
+    {
+        titolo:"Indirizzo",
+  items:[
+            "Agenzia Ruta del Sol",
+            "Ruta del Sol ",
+            "Via Treportina 11",
+            "30013 Cà Savio",
+            "Presso Merano",
+            "Italia"
+        ]
+},
+{
+   titolo:"Contatti",
+  items: [
+
+            "Tel: +39 0452 491233",
+            "info@rutadelsol.it",
+            "www.rutadelsol.it/it"
+        ]
+},
+{
+  titolo:"Servizi e novità",
+  items: [
+    
+            "Itinerari",
+            "Garanzie",
+            "MEdical Center Quellenhof",
+            "Lavorare al Quellenhof",
+            "Immagini",
+            "Webcam"   
+  ]
+},
+{
+ titolo: "Informazioni",
+ items: [
+            "Chi Siamo",
+            "Privacy e sicurezza",
+            "Assistenza"
+        ]
+},
+{
+ titolo: "Recensioni",
+ items: [
+            
+            "92% di 100%",
+            "più di 100.000 recensioni",
+        ]
+},
+]
+
+@social = [
+  { name: "facebook", icon: "/images/facebook.png" },
+  { name: "instagram", icon: "/images/instagram.png" },
+  { name: "youtube", icon: "/images/youtube.png" },
+  { name: "tiktok", icon: "/images/tiktok.png" },
+  { name: "twitter", icon: "/images/twitter.png" }
 ]
 end
 end
